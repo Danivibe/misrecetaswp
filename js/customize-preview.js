46c73e47db36617e2ac756780106fe96c43f3163
Daniel V <77021190+Danivibe@users.noreply.github.com>
2021-02-10 20:44:06 +0100

    Archivos Iniciales WP

--- js/customize-preview.js
+++ js/customize-preview.js
@@ -1,60 +1,62 @@
-/**
- * File customize-preview.js.
- *
- * Theme Customizer enhancements for a better user experience.
- *
- * Contains handlers to make Theme Customizer preview reload changes asynchronously.
- */
-
-(function( $ ) {
-
-	// Primary color.
-	wp.customize( 'primary_color', function( value ) {
+/* global twentytwentyoneGetHexLum, jQuery */
+( function() {
+	// Add listener for the "background_color" control.
+	wp.customize( 'background_color', function( value ) {
 		value.bind( function( to ) {
-			// Update custom color CSS.
-			var style = $( '#custom-theme-colors' ),
-				hue = style.data( 'hue' ),
-				css = style.html(),
-				color;
+			var lum = twentytwentyoneGetHexLum( to ),
+				isDark = 127 > lum,
+				textColor = ! isDark ? 'var(--global--color-dark-gray)' : 'var(--global--color-light-gray)',
+				tableColor = ! isDark ? 'var(--global--color-light-gray)' : 'var(--global--color-dark-gray)',
+				stylesheetID = 'twentytwentyone-customizer-inline-styles',
+				stylesheet,
+				styles;
 
-			if( 'custom' === to ){
-				// If a custom primary color is selected, use the currently set primary_color_hue.
-				color = wp.customize.get().primary_color_hue;
+			// Modify the html & body classes depending on whether this is a dark background or not.
+			if ( isDark ) {
+				document.body.classList.add( 'is-dark-theme' );
+				document.documentElement.classList.add( 'is-dark-theme' );
+				document.body.classList.remove( 'is-light-theme' );
+				document.documentElement.classList.remove( 'is-light-theme' );
+				document.documentElement.classList.remove( 'respect-color-scheme-preference' );
 			} else {
-				// If the "default" option is selected, get the default primary_color_hue.
-				color = 199;
+				document.body.classList.remove( 'is-dark-theme' );
+				document.documentElement.classList.remove( 'is-dark-theme' );
+				document.body.classList.add( 'is-light-theme' );
+				document.documentElement.classList.add( 'is-light-theme' );
+				if ( wp.customize( 'respect_user_color_preference' ).get() ) {
+					document.documentElement.classList.add( 'respect-color-scheme-preference' );
+				}
 			}
 
-			// Equivalent to css.replaceAll, with hue followed by comma to prevent values with units from being changed.
-			css = css.split( hue + ',' ).join( color + ',' );
-			style.html( css ).data( 'hue', color );
-		});
-	});
-
-	// Primary color hue.
-	wp.customize( 'primary_color_hue', function( value ) {
-		value.bind( function( to ) {
-
-			// Update custom color CSS.
-			var style = $( '#custom-theme-colors' ),
-				hue = style.data( 'hue' ),
-				css = style.html();
-
-			// Equivalent to css.replaceAll, with hue followed by comma to prevent values with units from being changed.
-			css = css.split( hue + ',' ).join( to + ',' );
-			style.html( css ).data( 'hue', to );
-		});
-	});
-
-	// Image filter.
-	wp.customize( 'image_filter', function( value ) {
-		value.bind( function( to ) {
-			if ( to ) {
-				$( 'body' ).addClass( 'image-filters-enabled' );
+			// Toggle the white background class.
+			if ( 225 <= lum ) {
+				document.body.classList.add( 'has-background-white' );
 			} else {
-				$( 'body' ).removeClass( 'image-filters-enabled' );
+				document.body.classList.remove( 'has-background-white' );
 			}
+
+			stylesheet = jQuery( '#' + stylesheetID );
+			styles = '';
+			// If the stylesheet doesn't exist, create it and append it to <head>.
+			if ( ! stylesheet.length ) {
+				jQuery( '#twenty-twenty-one-style-inline-css' ).after( '<style id="' + stylesheetID + '"></style>' );
+				stylesheet = jQuery( '#' + stylesheetID );
+			}
+
+			// Generate the styles.
+			styles += '--global--color-primary:' + textColor + ';';
+			styles += '--global--color-secondary:' + textColor + ';';
+			styles += '--global--color-background:' + to + ';';
+
+			styles += '--button--color-background:' + textColor + ';';
+			styles += '--button--color-text:' + to + ';';
+			styles += '--button--color-text-hover:' + textColor + ';';
+
+			styles += '--table--stripes-border-color:' + tableColor + ';';
+			styles += '--table--stripes-background-color:' + tableColor + ';';
+
+			// Add the styles.
+			stylesheet.html( ':root{' + styles + '}' );
 		} );
 	} );
-
-})( jQuery );
+}() );
